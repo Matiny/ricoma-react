69f5c328d4a2bcbf3a479af41dd12461d3c70e5c
Matiny <matinylouis@gmail.com>
2019-12-11 17:02:11 -0500

    Updates

---
+++ precache-manifest.d340ca314a476e1d4067de0cbf4614b4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28306c927c3a4d3a1d2f09ee490e776",
+    "url": "/ricoma-react/index.html"
+  },
+  {
+    "revision": "46956e9a6487226cb935",
+    "url": "/ricoma-react/static/css/main.f8edabbc.chunk.css"
+  },
+  {
+    "revision": "a4dc28286fce2716f891",
+    "url": "/ricoma-react/static/js/2.a3b9b1ca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ricoma-react/static/js/2.a3b9b1ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46956e9a6487226cb935",
+    "url": "/ricoma-react/static/js/main.dd2ece79.chunk.js"
+  },
+  {
+    "revision": "6a8af82b12caf89368c8",
+    "url": "/ricoma-react/static/js/runtime-main.393962b5.js"
+  },
+  {
+    "revision": "5d6719a48fa08f6b056fc85110895f25",
+    "url": "/ricoma-react/static/media/airpods.5d6719a4.jpg"
+  },
+  {
+    "revision": "e31f9a561a30fec4d4efb79b1d66c028",
+    "url": "/ricoma-react/static/media/arrow-down.e31f9a56.svg"
+  },
+  {
+    "revision": "93010bdd91ba859905ec3692f7d7319e",
+    "url": "/ricoma-react/static/media/cart.93010bdd.svg"
+  },
+  {
+    "revision": "873610f542f0c1eb5180fdea31301ea6",
+    "url": "/ricoma-react/static/media/chair.873610f5.jpg"
+  },
+  {
+    "revision": "e612b013fd7b6dffeb26b0d8053e0cb8",
+    "url": "/ricoma-react/static/media/check.e612b013.svg"
+  },
+  {
+    "revision": "f8b7d8e28e5d0574754f20e991444353",
+    "url": "/ricoma-react/static/media/close.f8b7d8e2.svg"
+  },
+  {
+    "revision": "29d5ae156a28dae22c277041ed8015a2",
+    "url": "/ricoma-react/static/media/footer.29d5ae15.svg"
+  },
+  {
+    "revision": "c0dd9600e6f5a750e6c26c1096782a96",
+    "url": "/ricoma-react/static/media/glasses.c0dd9600.jpg"
+  },
+  {
+    "revision": "3f638ef6aa6414ac1799f00d43b59d8a",
+    "url": "/ricoma-react/static/media/hero-bg-mobile.3f638ef6.jpg"
+  },
+  {
+    "revision": "348f09c7cd783e627bad5a2cf1f4feee",
+    "url": "/ricoma-react/static/media/hero-bg.348f09c7.png"
+  },
+  {
+    "revision": "35e16622286b31a190c78d43234c4b36",
+    "url": "/ricoma-react/static/media/hero.35e16622.png"
+  },
+  {
+    "revision": "c275e5189ecaabdbfdc5407dc031c28b",
+    "url": "/ricoma-react/static/media/menu.c275e518.svg"
+  },
+  {
+    "revision": "56a5bb662fecbcb3fd12fc0f9af7e35e",
+    "url": "/ricoma-react/static/media/plant.56a5bb66.jpg"
+  }
+]);
